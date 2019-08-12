7d5bbe3dc352201032c30bc9bbbc717191a7b59d
Thabo <thabo.ngubane@primafin.co.za>
2019-08-12 14:13:58 +0200

    Fix checking of common deps package existance

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,7 @@
 const spawn = require('cross-spawn');
 const path = require('path');
 const commonUtils = require('../lib/utils/index').common;
+const shell = require('shelljs');
 
 const resolveScript = (scriptName) => require.resolve(`../lib/scripts/${scriptName}`);
 
@@ -46,24 +47,27 @@ const spawnScriptProcess = ({ scriptName, commonDepsSourcePackageName = null, is
       stringColor: 'magenta',
       stringStyle: 'underline'
     });
-
-    spawn.sync(
-      'bash',
-      [path.join(__dirname, `../lib/scripts/${scriptName}.sh`)]
-    );
-  }
+    console.log('About to sparl teh bash script');
+    shell.exec(`bash ${path.join(__dirname, `../lib/scripts/${scriptName}.sh`)}`);
+    // spawn.sync(
+    //   'bash',
+    //   [path.join(__dirname, `../lib/scripts/${scriptName}.sh`)]
+    // );
+}
 };
 
 const processScriptName = process.argv[2];
 
 switch(processScriptName) {
   case scriptNames.preInstall:
+    console.log(process.argv);
     spawnScriptProcess({ scriptName: processScriptName, commonDepsSourcePackageName: process.argv[3] });
     break;
   case scriptNames.postInstall:
     spawnScriptProcess({ scriptName: processScriptName });
     break;
   case scriptNames.installIFS:
+    console.log(process.argv, scriptNames.installIFS);
     spawnScriptProcess({ scriptName: processScriptName, isShellScript: true });
     break;
   case scriptNames.extendDependencies:
